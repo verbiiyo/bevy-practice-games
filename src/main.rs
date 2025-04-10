02e2ae7db0a520a46db186dafcdfaad0d05907dc
verbiiyo <dyllanmccreary@gmail.com>
2025-04-10 13:23:37 -0700

    hhhhhhuuhh

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,36 @@
-use bevy::prelude::*;
+use bevy::{color::palettes::basic::WHITE, prelude::*};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, draw_cursor)
         .run();
 }
 
+fn draw_cursor(
+    camera_query: Single<(&Camera, &GlobalTransform)>,
+    windows: Query<&Window>,
+    mut gizmos: Gizmos,
+) {
+    let (camera, camera_transform) = *camera_query;
+
+    let Ok(window) = windows.get_single() else {
+        return;
+    };
+
+    let Some(cursor_position) = window.cursor_position() else {
+        return;
+    };
+
+    // Calculate a world position based on the cursor's position.
+    let Ok(point) = camera.viewport_to_world_2d(camera_transform, cursor_position) else {
+        return;
+    };
+
+    gizmos.circle_2d(point, 10., WHITE);
+}
+
 fn setup(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera2d);
 }
